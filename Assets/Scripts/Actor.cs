0df51ff98c9b9761933b8bc9e4110f89d3c353d2
atouchetAIE <cidleaf@gmail.com>
2016-03-02 14:00:22 -0600

    Actors

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 
 public class Actor : MonoBehaviour {
+    protected float speed;
+    protected int health;
+    protected int bullDam;
 
-	// Use this for initialization
-	void Start () {
+
+
+    // Use this for initialization
+    protected virtual void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	protected virtual void Update () {
+	    if(this.health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
 	}
+
+    protected virtual void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            health -= bullDam;
+        }
+    }
 }

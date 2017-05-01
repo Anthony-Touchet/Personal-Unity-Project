7bbc17d617443c1e51a74ce31fcc653c166896ca
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-05-01 15:56:00 -0500

    Start on Conversation

---
+++ Assets/Scripts/Conversation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conversation : MonoBehaviour
+{
+    private AudioSource m_AudioSource;
+    private Line m_CurrentLine;
+
+    public List<Line> conversationLines = new List<Line>();
+
+    void Awake()
+    {
+		
+	}
+	
+	void Update()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/LineData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Line
+{
+    public AudioClip sourceClip;
+    public string line;
+
+    public virtual void Play(string text) {}
+}
+
+public class InstantLine : Line { }
+
+public class BranchingLine : Line
+{
+    public class ReactionLine
+    {
+        public Line initalLine;
+        public Line reactionLine;
+    }
+
+    public List<ReactionLine> reactionLines = new List<ReactionLine>();
+
+    public override void Play(string text)
+    {
+        
+    }
+
+    public void PlayChoice(int choice, string text)
+    {
+        
+    }
+}

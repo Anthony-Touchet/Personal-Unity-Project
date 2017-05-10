c6b428fb34e83f5148439aac4dd739f013a28d7a
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-05-10 11:49:21 -0500

    Forgot this

--- Assets/Scripts/LineData.cs
+++ Assets/Scripts/LineData.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class Line
+[CreateAssetMenu(menuName = "Dialogue/Line", fileName = "Line")]
+public class Line : ScriptableObject
 {
     public AudioClip sourceClip;
     public string line;
@@ -14,17 +15,19 @@ public class Line
 [Serializable]
 public class Reaction
 {
-    public Line initalLine;
+    public Line playerLine;
     public Line reactionLine;
 }
 
 [Serializable]
+[CreateAssetMenu(menuName = "Dialogue/Branching Line", fileName = "Branching Line")]
 public class BranchingLine : Line
 {
     public List<Reaction> reactions = new List<Reaction>();
 }
 
 [Serializable]
+[CreateAssetMenu(menuName = "Dialogue/Hub Line", fileName = "Hub Line")]
 public class HubLine : Line
 {
     public List<Reaction> choicesList = new List<Reaction>();

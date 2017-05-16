3ff3e2a9a4c4582c8e6b304d9c42eac662424018
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-05-16 17:52:59 -0500

    Stats and a mess up
    Started defining stats. Scriptables got messed up in the process

---
+++ Assets/Scripts/Stats/AttributeInformation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+namespace Stats
+{
+    public enum AttributeType
+    {
+        STRENGTH,
+        DEXTERITY,
+        CONSTITUTION,
+        INTELLIGENCE,
+        WISDOM,
+        CHARISMA
+    }
+
+    public enum Skill
+    {
+        ACROBATICS,
+        ANIMAL_HANDLING,
+        ARCANA,
+        ATHLETICS,
+        DECEPTION,
+        HISTORY,
+        INSIGHT,
+        INTIMIDATION,
+        INVESTIGATION,
+        MEDICINE,
+        NATURE,
+        PERCEPTION,
+        PREFORMANCE,
+        PERSUASION,
+        RELIGION,
+        SLIGHT_OF_HAND,
+        STEALTH,
+        SURVIVAL,
+    }
+
+    [Serializable]
+    public class Attribute
+    {
+        [SerializeField]
+        private int m_Magnitude;
+        [SerializeField]
+        private AttributeType m_AttributeType;
+
+        public int Magnitude { get { return m_Magnitude; } }
+        public AttributeType AttributeType { get { return m_AttributeType; } }
+        public int AttributeBonus { get { return (Magnitude - 10) / 2; } }
+
+        public Attribute(AttributeType pAttributeType, int pMagnitude)
+        {
+            m_Magnitude = pMagnitude;
+            m_AttributeType = pAttributeType;
+        }
+    }
+
+    public abstract class Ability
+    {
+        public float magnitude;
+
+        public virtual object Execute()
+        {
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Stats/Unit.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Stats
+{
+    [CreateAssetMenu(menuName = "Unit")]
+    public class Unit : ScriptableObject
+    {
+        [SerializeField]
+        private List<Attribute> attributes;
+        [SerializeField]
+        private List<Skill> proficentcySkills;
+
+        private int level = 1;
+
+        public int ProficentcyBonus
+        {
+            get { return level/5 + 2; }
+        }
+
+        public Unit(int str, int dex, int con, int intell, int wis, int cha)
+        {
+            attributes = new List<Attribute>
+            {
+                new Attribute(AttributeType.STRENGTH, str),
+                new Attribute(AttributeType.DEXTERITY, dex),
+                new Attribute(AttributeType.CONSTITUTION, con),
+                new Attribute(AttributeType.INTELLIGENCE, intell),
+                new Attribute(AttributeType.WISDOM, wis),
+                new Attribute(AttributeType.CHARISMA, cha)
+            };
+        }
+
+        public Attribute ReturnSkill(AttributeType attributeType)
+        {
+            return attributes.FirstOrDefault(attribute => attribute.AttributeType == attributeType);
+        }
+
+        public bool IsProficentInSkill(Skill skill)
+        {
+            return proficentcySkills.Contains(skill);
+        }
+
+        public List<int> RollDice(uint range, uint quantity)
+        {
+            var rolls = new List<int>();
+            var r  = new System.Random();
+
+            for (var i = 0; i < quantity; i++)
+            {
+                rolls.Add(r.Next(1, (int)range + 1));
+            }
+
+            return rolls;
+        }
+
+        public List<int> RollDice(uint range, uint quantity, Attribute attribute)
+        {
+            var rolls = new List<int>();
+            var r = new System.Random();
+
+            for (var i = 0; i < quantity; i++)
+            {
+                var roll = r.Next(1, (int) range + 1);
+                roll += attribute.AttributeBonus;
+                rolls.Add(roll);
+            }
+
+            return rolls;
+        }
+
+        public List<int> RollDice(uint range, uint quantity, Attribute attribute, Skill skill)
+        {
+            var rolls = new List<int>();
+            var r = new System.Random();
+
+            for (var i = 0; i < quantity; i++)
+            {
+                var roll = r.Next(1, (int)range + 1);
+                roll += attribute.AttributeBonus;
+                if (IsProficentInSkill(skill))
+                    roll += ProficentcyBonus;
+                rolls.Add(roll);
+            }
+
+            return rolls;
+        }
+    }
+}

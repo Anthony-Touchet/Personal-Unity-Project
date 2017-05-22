e5b52a7391722c3b9c03a5c7a2682299d7cf63a2
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-05-22 09:40:24 -0500

    Start of Action Line

---
+++ Assets/Scripts/Dialogue/ActionLine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dialogue
+{
+    [Serializable]
+    [CreateAssetMenu(menuName = "Dialogue/Action Line", fileName = "Action Line")]
+    public class ActionLine : Line
+    {
+        
+    }
+}

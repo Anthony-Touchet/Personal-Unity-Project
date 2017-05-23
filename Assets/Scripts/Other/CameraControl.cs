a1e60c17748b4927f409dfe511bde70317a213b6
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-05-23 16:53:37 -0500

    More Console Commands

--- Assets/Scripts/Other/CameraControl.cs
+++ Assets/Scripts/Other/CameraControl.cs
@@ -3,15 +3,30 @@
 public class CameraControl : MonoBehaviour
 {
     private Transform m_CameraTransform;
-    public float speed;
+    public float rotationSpeed;
+    public float movementSpeed;
+    public float scrollSpeed = 1;
 
-	private void Awake ()
+    private void Awake ()
 	{
 	    m_CameraTransform = transform.GetComponentInChildren<Camera>().transform;   // Get Camera Transform
 	}
 
     private void Update ()
     {
+        var movement = new Vector3();
+
+        // Get Regular movement
+        movement += m_CameraTransform.forward * Input.GetAxisRaw("Vertical");
+        movement += m_CameraTransform.right * Input.GetAxisRaw("Horizontal");
+
+        // Get Scroll Movement and scale it
+        var mouseWheel = m_CameraTransform.forward * Input.GetAxis("Mouse ScrollWheel") * scrollSpeed *
+            Time.deltaTime * 10;
+
+        // Scale Movement and add Mouse scroll
+        transform.position += (movement.normalized * movementSpeed * Time.deltaTime) + mouseWheel;
+
         // If button not clicked, Don't Rotate
         if (Input.GetAxis("Fire2") <= 0)
             return;
@@ -20,9 +35,9 @@ public class CameraControl : MonoBehaviour
         var vertical = Input.GetAxis("Mouse Y");    // how much is mouse moving on the Y
 
         // Rotate this transform on it's Y based off X mouse movement
-        transform.Rotate(Vector3.up, horizontal * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up, horizontal * rotationSpeed * Time.deltaTime);
 
         // Rotate Camera transform on it's X based off Y mouse movement
-        m_CameraTransform.Rotate(Vector3.right, -vertical * speed * Time.deltaTime);
+        m_CameraTransform.Rotate(Vector3.right, -vertical * rotationSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Other/CommandBehaviors.cs
+++ Assets/Scripts/Other/CommandBehaviors.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -8,21 +8,49 @@ public class CommandBehaviors : MonoBehaviour
 {
     private GameObject m_CurrentGameObject;
     private InputField m_InputField;
+    private bool m_CommandProptUp;
+    private CameraControl camControl;
+
+    public delegate void Command();
+    public delegate void CommandSingleParameter(string s);
+    public delegate void CommandThreeParameters(string s1, string s2, string s3);
+
+    private Dictionary<string, Command> commandDictionary = new Dictionary<string, Command>();
+    private Dictionary<string, CommandSingleParameter> singleParameters = 
+        new Dictionary<string, CommandSingleParameter>();
+    private Dictionary<string, CommandThreeParameters> threeParameters =
+       new Dictionary<string, CommandThreeParameters>();
+
 
     public Text executionText;
+    public GameObject canvasGameObject;
 
 	// Use this for initialization
 	private void Awake ()
 	{
-	    m_InputField = FindObjectOfType<InputField>();
+	    AddFunctions();
+        camControl = FindObjectOfType<CameraControl>();
+        m_InputField = FindObjectOfType<InputField>();
 	    m_CurrentGameObject = null;
 	    executionText.text = "";
+
+        canvasGameObject.SetActive(m_CommandProptUp);
 	}
 	
 	// Update is called once per frame
 	private void Update ()
-    {
-        // Get an object
+	{
+	    if (Input.GetKeyUp(KeyCode.BackQuote))
+	    {
+            m_CommandProptUp = !m_CommandProptUp;
+            canvasGameObject.SetActive(m_CommandProptUp);
+	        camControl.enabled = !m_CommandProptUp;
+	    }
+
+	    if (m_CommandProptUp == false)
+	        return;
+
+	    // Get an object
         if (Input.GetKeyUp(KeyCode.Mouse0) && EventSystem.current.currentSelectedGameObject == null)
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -34,32 +62,129 @@ public class CommandBehaviors : MonoBehaviour
                 return;
 
             m_CurrentGameObject = hit.transform.gameObject;
-            executionText.text += m_CurrentGameObject.name + " was selected. \n";
+            UpdateBoard(m_CurrentGameObject.name + " was selected.\n");
         }
 	}
 
-    public void CommandExecute(string command)
+    public void CommandExecute()
     {
-        var history = "";
-        switch (m_InputField.text)
+        var command = m_InputField.text;
+        var history = command + "\n";
+        var parameters = m_InputField.text.Split(' ').ToList();
+        switch (parameters.Count)
         {
-            case "Destroy":
+            case 1:
                 try
                 {
-                    Destroy(m_CurrentGameObject);
-                    history += m_CurrentGameObject.name + " was destroyed\n";
+                    commandDictionary[parameters[0]].Invoke();
                 }
                 catch
+                { history += "ERROR!!\n"; }
+                break;
+
+            case 2:
+                try
                 {
-                    history += "No Gameobject Selected. \n";
+                    singleParameters[parameters[0]].Invoke(parameters[1]);
                 }
-                
+                catch
+                { history += "ERROR!!\n"; }
+                break;
+
+            case 4:
+                try
+                {
+                    threeParameters[parameters[0]].Invoke(parameters[1], parameters[2], parameters[3]);
+                }
+                catch
+                { history += "ERROR!!\n"; }
                 break;
 
             default:
-                history += "Command not found \n";
+                history += "ERROR!!\n";
                 break;
         }
-        executionText.text += history;
+
+        UpdateBoard(history); 
+    }
+
+    public void UpdateBoard(string data)
+    {
+        var textHeight = executionText.rectTransform.rect.height - 
+            (executionText.rectTransform.rect.height * .15f);
+
+        var numberOfLines = (int)(textHeight / executionText.fontSize);
+
+        if (executionText.text.Count(c => c == '\n') >= numberOfLines - 1)
+        {
+            executionText.text = "";
+        }
+        executionText.text += data;
+
+    }
+
+    private void AddFunctions()
+    {
+        commandDictionary.Add("destroy", DestroyCurrent);
+        commandDictionary.Add("rotate", RotateCurrent);
+        commandDictionary.Add("color", ApplyRandomColor);
+
+        singleParameters.Add("rotate", RotateCurrent);
+        singleParameters.Add("color", ApplyRandomColor);
+        singleParameters.Add("spawn", Spawn);
+
+        threeParameters.Add("rotate", RotateCurrent);
+        threeParameters.Add("moveposition", MovePosition);
+    }
+
+    private void RotateCurrent()
+    {
+        var r = m_CurrentGameObject.AddComponent<RotateGameObject>();
+        r.angle = Vector3.up * 90;
+    }
+
+    private void RotateCurrent(string s)
+    {
+        var mag = float.Parse(s);
+        var r = m_CurrentGameObject.AddComponent<RotateGameObject>();
+        r.angle = Vector3.up * mag;
+    }
+
+    private void RotateCurrent(string s1, string s2, string s3)
+    {
+        var x = float.Parse(s1);
+        var y = float.Parse(s2);
+        var z = float.Parse(s3);
+        var r = m_CurrentGameObject.AddComponent<RotateGameObject>();
+        r.angle = new Vector3(x,y,z);
+    }
+
+    private void DestroyCurrent()
+    {
+        Destroy(m_CurrentGameObject);
+    }
+
+    private void ApplyRandomColor()
+    {
+        var c = m_CurrentGameObject.AddComponent<RandomColor>();
+        c.waitTime = .1f;
+    }
+
+    private void ApplyRandomColor(string wait)
+    {
+        var waitTime = float.Parse(wait);
+        var c = m_CurrentGameObject.AddComponent<RandomColor>();
+        c.waitTime = waitTime;
+    }
+
+    private void MovePosition(string x, string y, string z)
+    {
+        var pos = new Vector3(float.Parse(x), float.Parse(y), float.Parse(z));
+        m_CurrentGameObject.transform.position = pos;
+    }
+
+    private void Spawn(string path)
+    {
+        Instantiate(Resources.Load(path));
     }
 }
